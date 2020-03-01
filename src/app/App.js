f3463c59b515ef6d1d0ca226ab6b08d1a0255141
Beimar Huarachi <beimar020@gmail.com>
2020-03-01 10:27:27 -0400

    Add base app layour

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
 import { useTranslation } from 'react-i18next';
+import NavBar from 'app/core/components/NavBar';
 import AppRouter from './AppRouter';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   const { t } = useTranslation();
   return (
     <>
-      <Header>{t('app-name')}</Header>
-      <AppRouter />
+      <NavBar />
+      <Container>
+        <AppRouter />
+      </Container>
     </>
   );
 }
